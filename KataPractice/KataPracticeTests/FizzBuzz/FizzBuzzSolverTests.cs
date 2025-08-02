3ddc49d70c3ae347c70584f0eab07b3f18c0948c
Oliver <info@oliver-fries.de>
2025-08-02 22:43:09 +0200

    RED Test for smaller than 1

--- KataPractice/KataPracticeTests/FizzBuzz/FizzBuzzSolverTests.cs
+++ KataPractice/KataPracticeTests/FizzBuzz/FizzBuzzSolverTests.cs
@@ -6,9 +6,15 @@ namespace KataPracticeTests.FizzBuzz;
 public class FizzBuzzSolverTests
 {
     [Fact]
-    public void ReturnsNumberAsString()
+    public void Run_ReturnsNumberAsString()
     {
         new FizzBuzzSolver().Run(1).ShouldBe("1");
     }
-    
+
+    [Fact]
+    public void Run_ReturnsHashtag_WhenNumberIsSmallerThanOne()
+    {
+        new FizzBuzzSolver().Run(-1).ShouldBe("#");
+    }
+
 }
