17e6ac54352d8b58bfec58cc011c9ac82ab467b7
Oliver <info@oliver-fries.de>
2025-08-06 22:23:36 +0200

    GREEN Implement FizzRule

--- KataPractice/KataPractice/FIzzBuzz/RuleBased/FIzzRule.cs
+++ KataPractice/KataPractice/FIzzBuzz/RuleBased/FIzzRule.cs
@@ -2,8 +2,11 @@ namespace KataPractice.FIzzBuzz.RuleBased;
 
 public class FizzRule
 {
+    private const int    Divisor     = 3;
+    private const string Replacement = "Fizz";
+
     public string Apply(int number)
     {
-        throw new NotImplementedException();
+        return number % Divisor == 0 ? Replacement : string.Empty;
     }
 }
